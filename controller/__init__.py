50fc5db98595805870c7fa3f476d4b62134f4107
parag gupta <paraggupta@parags-MacBook-Air.local>
2025-02-22 04:19:10 +0530

    Tips Changes

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,6 +1,9 @@
+import time
 from flask import Flask
 import os
+import threading
 from flask_jwt_extended import JWTManager
+import requests
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads'
@@ -9,8 +12,20 @@ jwt = JWTManager(app)
 app.config['JWT_SECRET_KEY'] = 'your_secret_key' 
 
 @app.route("/")
-def hello_world():
+def hello_world():    
     return "<p>Hello, World!</p>"
+
+
+def auto_call_api():
+    print("Service Started")
+    while True:        
+        try:
+            response = requests.get("https://climacare-backend.onrender.com/")
+        except Exception as e:
+            print("Error calling API:", e)
+        time.sleep(3)
+        
+threading.Thread(target=auto_call_api, daemon=True).start()
 
 from controller.auth import *
 from controller.tips import *
